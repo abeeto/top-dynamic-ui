29c06b222e75bc28f10c3718a142ac7e982d8c4f
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-01-15 09:08:43 +0000

    Add navigation circle UI and select them along with images

--- src/giveCarouselBehavior.js
+++ src/giveCarouselBehavior.js
@@ -1,13 +1,18 @@
 function getImages(wrapper) {
   return Array.from(wrapper.querySelectorAll(".image"));
 }
+function getNavCircles(wrapper) {
+  return Array.from(wrapper.querySelectorAll(".nav-circle"));
+}
 function addNavCircles(wrapper, numOfCircles) {
-  const circlesWrapperNode = wrapper.querySelector(".nav-circles-wrapper");
+  const circlesWrapperNode = document.createElement("div");
+  circlesWrapperNode.classList.add("nav-circles-wrapper");
   for (let circleIndex = 0; circleIndex < numOfCircles; circleIndex += 1) {
     const circleToAdd = document.createElement("div");
     circleToAdd.classList.add("nav-circle");
     circlesWrapperNode.appendChild(circleToAdd);
   }
+  wrapper.appendChild(circlesWrapperNode);
 }
 
 function getImageControls(wrapper) {
@@ -19,43 +24,55 @@ function getImageControls(wrapper) {
   };
 }
 
-function selectNode(node) {
-  node.classList.add("selected");
+function deselectNodes(nodes) {
+  nodes.forEach((node) => node.classList.remove("selected"));
 }
 
-function deselectNode(node) {
-  node.classList.remove("selected");
-}
-function getIndexOfSelectedImage(images) {
-  return images.findIndex((image) => image.classList.contains("selected"));
+function selectNodes(nodes) {
+  nodes.forEach((node) => node.classList.add("selected"));
 }
 
-function goToNextImage(images) {
-  const pos = getIndexOfSelectedImage(images);
-  deselectNode(images[pos]);
-  if (pos + 1 !== images.length) {
-    selectNode(images[pos + 1]);
-  } else {
-    selectNode(images[0]);
+function findIndexOfFirstSelectedNode(nodesArray) {
+  return nodesArray.findIndex((node) => node.classList.contains("selected"));
+}
+
+function getCorrectIndex({ currentIndex, length, previous, next }) {
+  if (previous) {
+    return currentIndex > 0 ? currentIndex - 1 : length - 1;
   }
-}
-
-function goToPrevImage(images) {
-  const pos = getIndexOfSelectedImage(images);
-  deselectNode(images[pos]);
-  if (pos - 1 > -1) {
-    selectNode(images[pos - 1]);
-  } else {
-    selectNode(images[images.length - 1]);
+  if (next) {
+    return currentIndex < length - 1 ? currentIndex + 1 : 0;
   }
+  return undefined;
 }
 
 export default function giveCarouselBehavior({ carouselWrapperNode }) {
   const allImages = getImages(carouselWrapperNode);
   addNavCircles(carouselWrapperNode, allImages.length);
+  const indexOfSelected = findIndexOfFirstSelectedNode(allImages);
+  const allNavCircles = getNavCircles(carouselWrapperNode);
+  selectNodes([allNavCircles[indexOfSelected]]);
   const { prevImageControl, nextImageControl } =
     getImageControls(carouselWrapperNode);
 
-  prevImageControl.addEventListener("click", () => goToPrevImage(allImages));
-  nextImageControl.addEventListener("click", () => goToNextImage(allImages));
+  const traverseCarousel = ({ previous, next }) => {
+    const currentIndex = findIndexOfFirstSelectedNode(allImages);
+    const newIndex = getCorrectIndex({
+      currentIndex,
+      previous,
+      next,
+      length: allImages.length,
+    });
+    deselectNodes([allImages[currentIndex], allNavCircles[currentIndex]]);
+    selectNodes([allImages[newIndex], allNavCircles[newIndex]]);
+  };
+
+  prevImageControl.addEventListener("click", () => {
+    traverseCarousel({ previous: true, next: false });
+  });
+  nextImageControl.addEventListener("click", () => {
+    traverseCarousel({ previous: false, next: true });
+  });
+
+  setInterval(() => traverseCarousel({ previous: false, next: true }), 5000);
 }
